064aa989c11112e169dbe4d3c9d737b0f76d4bc3
moon920830 <lucasdavis0830@outlook.com>
2020-10-15 16:06:13 +0200

    add Services component

---
+++ src/components/Services.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Title from './Title'
+
+class Services extends Component {
+  render () {
+    return (
+      <div>
+        SERVICES
+      </div>
+    )
+  }
+}
+
+export default Component
