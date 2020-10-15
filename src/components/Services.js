c2f02df31847d2db953cf71a1eecdbfd91783276
moon920830 <lucasdavis0830@outlook.com>
2020-10-15 19:09:52 +0200

    Services component + integration

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,14 +1,51 @@
 import React, {Component} from 'react'
 import Title from './Title'
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa'
 
 class Services extends Component {
-  render () {
+  state = {
+    services: [
+      {
+        icon:<FaCocktail />,
+        title:"free cocktails",
+        info: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+      },
+      {
+        icon:<FaBeer />,
+        title:"unique, local brewed beer",
+        info: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+      },
+      {
+        icon:<FaHiking />,
+        title:"Hike on the beach",
+        info: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+      },
+      {
+        icon:<FaShuttleVan />,
+        title:"transport from/to the airport",
+        info: "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+      }
+    ]
+  }
+
+  render() {
     return (
-      <div>
-        SERVICES
-      </div>
+      <section className='services'>
+        <Title title='services' />
+        <div className='services-center'>
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} classname='service'>
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>  
+              </article>
+            )
+          })}
+        </div>
+      </section>
     )
   }
 }
 
-export default Component
+export default Services
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
-import {Link} from 'react-router-dom'
+import Services from '../components/Services'
 
 function Home () {
   return (
-    <Hero>
-      <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $299'>
-        <Link to='rooms' className='btn-primary'>
-          Our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $299'>
+          <Link to='rooms' className='btn-primary'>
+            Our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>  
   )
 }
 
