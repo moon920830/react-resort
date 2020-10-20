38795e13521532cc96e8a846f5753fcdbf66b4ea
moon920830 <lucasdavis0830@outlook.com>
2020-10-20 15:33:10 +0200

    context CP state & functions setting

--- src/context.js
+++ src/context.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react'
+import items from './data'
 
 const RoomContext = React.createContext()
 
 class RoomProvider extends Component {
-  state = {}
+  state = {
+    rooms: [],
+    sortedRooms: [],
+    feturedRooms: [],
+    loading: true
+  }
 
-  render () {
+  conmponentDidMount() {
+    let rooms = this.formatData(items)
+    let featuredRooms = rooms.filter(room => room.feature === true)
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false
+    })
+  }
+
+  formatData(items) {
+    let tempItems =  items.map(item => {
+      let id = item.sys.id
+      let images = item.fields.images.map(image => image.fields.file.url)
+      let room = {...item.fields, images, id}
+      return room
+    })
+    return tempItems
+  }
+
+  render() {
     return (
       <RoomContext.Provider value ={{...this.state}}>
         {this.props.children}
