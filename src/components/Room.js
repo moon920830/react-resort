9e0af1df01546adc09542366620a33ad1702cd5f
moon920830 <lucasdavis0830@outlook.com>
2020-10-23 10:07:34 +0200

    add Room infoName and propTypes

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import defaultImg from '../images/room-1.jpeg'
+import PropTypes from 'prop-types'
 
 function Room ({room}) {
-  const  {name, slug, images, price} = room
+  const {name, slug, images, price} = room
   return (
     <article className='room'>
       <div className='img-container'>
@@ -16,8 +17,20 @@ function Room ({room}) {
           Features
         </Link>
       </div>
+      <p className='room-info'>
+        {name}
+      </p>
     </article>
   )
+}
+
+Room.propTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(Proptypes.string).isRequired,
+    price: PropTypes.number.isRequired
+  })
 }
 
 export default Room
