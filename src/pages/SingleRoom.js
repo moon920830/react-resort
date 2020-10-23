718e12e2399fab0a0de37b85556dd1e98df98cf0
moon920830 <lucasdavis0830@outlook.com>
2020-10-23 15:36:21 +0200

    singleRoom cp !broken!

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,37 @@
-import { render } from "@testing-library/react";
 import React, {Component} from 'react'
+import {RoomContext} from '../context'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import defaultBcg from '../images/room-1.jpeg'
 
 class SingleRoom extends Component {
-  render() {
-    return (
+  constructor (props) {
+    super (props)
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg: defaultBcg
+    }
+  }
+
+  static contextType = RoomContext
+
+  render () {
+    const { getRoom } = this.context
+    const room = getRoom(this.state.slug)
+
+    if (!room) {
+      return (
+        <div className='error'>
+          <h3>room not found</h3>
+          <Link to='/rooms' className='btn-primary'>
+            Back to rooms
+          </Link> 
+        </div>
+      )
+    } else return (
       <div>
-        SINGLE ROOM
+        SINGLE ROOM {room.name}
       </div>
     )
   }
