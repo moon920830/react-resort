061a30ca3d66a66774b59ab1711c63d336681794
moon920830 <lucasdavis0830@outlook.com>
2020-10-23 10:10:12 +0200

    typo correction

--- src/components/Room.js
+++ src/components/Room.js
@@ -28,7 +28,7 @@ Room.propTypes = {
   room: PropTypes.shape({
     name: PropTypes.string.isRequired,
     slug: PropTypes.string.isRequired,
-    images: PropTypes.arrayOf(Proptypes.string).isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
     price: PropTypes.number.isRequired
   })
 }
