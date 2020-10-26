3dc3c291e7428e92a5b1025cfcd6c4f514ad9cb3
moon920830 <lucasdavis0830@outlook.com>
2020-10-26 09:55:26 +0100

    destructuring images array

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -34,14 +34,25 @@ class SingleRoom extends Component {
     
     const {name, desription, capacity, price, size, extras, breakfast, pets, images} = room
 
+    const [mainImg, ...defaultImg] = images
+
     return (
-      <StyledHero img={images[0] || this.state.defaultBcg}>
-        <Banner title={`${name} room`}>
-          <Link to='/rooms' className='btn-primary'>
-            Back to rooms
-          </Link>
-        </Banner>  
-      </StyledHero>
+      <>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to='/rooms' className='btn-primary'>
+              Back to rooms
+            </Link>
+          </Banner>  
+        </StyledHero>
+        <section className='single-room'>
+          <div className='single-room-images'>
+            {defaultImg.map((item, index) => {
+              return <img key={index} src={item} alt={name} />
+            })}
+          </div>
+        </section>
+      </>
     )
   }
 }
