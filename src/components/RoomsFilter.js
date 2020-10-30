5743bcfd16bd4ebb048a2ef393ebf445b41a35af
moon920830 <lucasdavis0830@outlook.com>
2020-10-30 11:33:45 +0100

    set JSX structure in RoomsFilter component

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {useContext} from 'react'
+import {RoomContext} from '../context'
+import Title from '../components/Title'
 
 function RoomsFilter () {
+  const context = useContext(RoomContext)
+  const {
+    handleChange, type, capacity, price, minPrice, maxPrice, minsize, maxSize, breakfeast, pets
+  } = context
+
   return (
-    <div>ROOMSFILTER</div>
+    <section className='filter-container'>
+      <Title title='search rooms'/>
+      <form className="filter-form">
+        <div className='form-group'>
+          <label htmlFor='type'>
+            room type
+          </label>
+          <select
+            name='type'
+            id='type'
+            value={type}
+            className='form-control'
+            onChange={handleChange}
+          />
+        </div>
+      </form>
+      ROOMSFILTER
+    </section>
   )
 }
 
