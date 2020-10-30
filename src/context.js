44c28d90843d4aca87a52882262244810dbc6d28
moon920830 <lucasdavis0830@outlook.com>
2020-10-30 11:03:57 +0100

    set maxPrice & maxSize default state values

--- src/context.js
+++ src/context.js
@@ -23,12 +23,17 @@ export default class RoomProvider extends Component {
   componentDidMount() {
     let rooms = this.formatData(items)
     let featuredRooms = rooms.filter(room => room.featured === true)
+    let maxPrice = Math.max(...rooms.map(item => item.price))
+    let maxSize = Math.max(...rooms.map(item => item.size))
 
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice: maxPrice,
+      maxSize: maxSize
     })
   }
 
