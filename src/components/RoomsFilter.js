c7364327cb9219702d15fd1104d593d1b3d245a7
moon920830 <lucasdavis0830@outlook.com>
2020-10-30 12:08:56 +0100

    add logic for unique 'Room Type' dropdown values

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -3,11 +3,23 @@ import {useContext} from 'react'
 import {RoomContext} from '../context'
 import Title from '../components/Title'
 
-function RoomsFilter () {
+/* Get unique values for dropdown menu options */
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))]
+}
+
+function RoomsFilter ({rooms}) {
   const context = useContext(RoomContext)
   const {
     handleChange, type, capacity, price, minPrice, maxPrice, minsize, maxSize, breakfeast, pets
   } = context
+
+  /*get unique types, add 'all', return JSX options */
+  let types = getUnique(rooms,'type')
+  types = ['all', ...types]
+  types = types.map((item, index) => {
+    return <option value={item} key={index}>{item}</option>
+  })
 
   return (
     <section className='filter-container'>
@@ -23,7 +35,9 @@ function RoomsFilter () {
             value={type}
             className='form-control'
             onChange={handleChange}
-          />
+          >
+            {types}
+          </select>
         </div>
       </form>
       ROOMSFILTER
