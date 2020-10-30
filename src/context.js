678d1fc72577165c22239f02181c18a4ed388bdf
moon920830 <lucasdavis0830@outlook.com>
2020-10-30 15:00:26 +0100

    add handleChange fct to context provider

--- src/context.js
+++ src/context.js
@@ -65,7 +65,11 @@ export default class RoomProvider extends Component {
 
   render() {
     return (
-      <RoomContext.Provider value ={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider value ={{
+        ...this.state,
+        getRoom: this.getRoom,
+        handleChange: this.handleChange
+      }}>
         {this.props.children}
       </RoomContext.Provider>
     )
