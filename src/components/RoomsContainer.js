c2bd32fed9307b40cba024319312302bacdcf588
moon920830 <lucasdavis0830@outlook.com>
2020-10-30 09:25:38 +0100

    <RoomContainer> access context /w render props

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import RoomsFilter from './RoomsFilter'
 import RoomsList from './RoomsList'
+import {RoomConsumer} from '../context'
+import Loading from './Loading'
 
 function RoomContainer () {
   return (
-    <>
-      <div>ROOMCONTAINER</div>
-      <RoomsFilter></RoomsFilter>
-      <RoomsList></RoomsList>
-    </>
+    <RoomConsumer>
+      {value => {
+        const {Loading, sortedRooms, rooms} = value
+        if (Loading) {
+          return <Loading />
+        }
+        return (
+          <div>
+            <RoomsFilter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} />
+          </div>
+        )
+      }}  
+    </RoomConsumer>
   )
 }
 
